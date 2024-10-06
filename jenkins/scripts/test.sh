34a00f1391555fa32fd2334814f65564fbf41df1
AlisinaY <yozbashi.a.y@gmail.com>
2024-10-06 20:07:47 +0200

    Add TEST.sh

---
+++ jenkins/scripts/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Exit script if any command fails
+set -e
+
+# Set environment variables
+export NODE_ENV=test
+
+# Run migrations or seed data if necessary
+# echo "Running migrations..."
+# npm run migrate
+
+# Run unit tests
+echo "Running unit tests..."
+npm test
