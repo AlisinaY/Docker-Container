492db45cbf902544edef5a2270e4082ae51fde26
AlisinaY <yozbashi.a.y@gmail.com>
2024-10-06 20:13:22 +0200

    ausführerbar gemacht aa

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-chmod +x jenkins/scripts/test.sh
-
-
-# Exit script if any command fails
-set -e
-
-# Set environment variables
-export NODE_ENV=test
-
-# Run migrations or seed data if necessary
-# echo "Running migrations..."
-# npm run migrate
-
-# Run unit tests
-echo "Running unit tests..."
-npm test
+echo "Hello Jenkins from test.sh"
