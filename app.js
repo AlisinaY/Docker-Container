86dcdfb043066b0b6f717c0527a3cb52d7a88691
AlisinaY <yozbashi.a.y@gmail.com>
2024-10-06 11:08:12 +0200

    Testing Application

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require("path")
-const bodyParser = require('body-parser');
-const todoRoutes = require('./routes/todos');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const bodyParser = require("body-parser");
+const todoRoutes = require("./routes/todos");
 
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect('mongodb://admin:password@mongodb/myDB?authSource=admin')
-.then(() => {
-  console.log('Successfully connected to MongoDB');
-})
-.catch(err => {
-  console.error('Connection error', err);
-});
+mongoose
+  .connect("mongodb://admin:password@mongodb/myDB?authSource=admin")
+  .then(() => {
+    console.log("Successfully connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+  });
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('views', path.join(__dirname, 'views'));
-app.set("view engine", "ejs")
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 // Routes
-app.use('/todos', todoRoutes);
+app.use("/todos", todoRoutes);
 
-app.get('/', (req, res) => {
-  console.log("Hello Ali Sina and how are you doing today?");
-  console.log("Hello Alisina, and how are you doing today?");
-  res.redirect('/todos');
+app.get("/", (req, res) => {
+  console.log("Just For Test");
+  res.redirect("/todos");
 });
 
 // Start the server
